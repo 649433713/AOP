f4ca745c96638cbccb383404ecb42b27657ce85a
旖旎丶F <649433713@qq.com>
2017-11-06 01:19:27 +0800

    Update SpringbootdemoApplication.java

--- src/main/java/yinywf/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/yinywf/springbootdemo/SpringbootdemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "yinywf.springbootdemo.dataobject.mapper")
+@EnableCaching
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
