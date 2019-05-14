aa5782c3f24cedaeddd75bb143fc64b21dae6af4
yinywf <wangfan@superid.cn>
2019-05-14 11:04:24 +0800

    SpringBoot 2.0 + jdk12

--- src/main/java/yinywf/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/yinywf/springbootdemo/SpringbootdemoApplication.java
@@ -1,12 +1,15 @@
 package yinywf.springbootdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan(basePackages = "yinywf.springbootdemo.dataobject.mapper")
 @EnableCaching
-public class SpringbootdemoApplication {
+public class SpringbootdemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
