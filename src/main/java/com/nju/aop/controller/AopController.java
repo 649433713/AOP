f9d135a33bb72d5cd1100029668d1463d30eaeae
Kimone <1799215066@qq.com>
2020-01-07 14:16:19 +0800

    环境诊断

--- src/main/java/com/nju/aop/controller/AopController.java
+++ src/main/java/com/nju/aop/controller/AopController.java
@@ -1,13 +1,18 @@
 package com.nju.aop.controller;
 
 import com.nju.aop.dataobject.Aop;
+import com.nju.aop.dataobject.Chain;
 import com.nju.aop.repository.AopRepository;
+import com.nju.aop.repository.ChainRepository;
 import com.nju.aop.utils.ExampleMatcherUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author yinywf
@@ -20,6 +25,8 @@ public class AopController {
 
     @Autowired
     private AopRepository aopRepository;
+    @Autowired
+    private ChainRepository chainRepository;
 
     @PostMapping("/search/findByExample")
     public Page<Aop> findByExample(@RequestBody Aop aop, Pageable pageable) {
@@ -33,4 +40,10 @@ public class AopController {
         return aopRepository.findAll(ExampleMatcherUtil.transfer(aop), pageable);
     }
 
+    @GetMapping("/findByAOId/{AOId}")
+    public List<Aop> findByAOId(@PathVariable Integer AOId) {
+        List<Chain> chains = chainRepository.findByEventIdAndType(AOId,"AdverseOutcome");
+        List<Aop> aops = aopRepository.findAllById(chains.stream().map(Chain::getAopId).collect(Collectors.toSet()));
+        return aops;
+    }
 }
--- src/main/java/com/nju/aop/repository/EventRepository.java
+++ src/main/java/com/nju/aop/repository/EventRepository.java
@@ -4,9 +4,11 @@ package com.nju.aop.repository;
 import com.nju.aop.dataobject.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author yinywf
  */
 public interface EventRepository extends JpaRepository<Event, Integer> {
-
+    List<Event> findByIdOrTitleLikeOrChineseLike(Integer id, String title, String chinese);
 }
